8293ef20321c5155bd1043a05adad8a4b05d3703
Ahmed <4hmed.jama@gmail.com>
2023-09-02 22:43:06 +0300

    changed header style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,7 @@
-
 const Header = () => {
-
-  // you can also but styles in a variable
-  const h1Style = {
-    fontSize: '3rem',
-    padding: '0.3em'
-  }
-
   return (
-
-    // inline style
-    // Note: its look like an js object
-    <header style={{
-      backgroundColor: 'black',
-      color: '#fff',
-      borderBottomLeftRadius: '1rem',
-      borderBottomRightRadius: '1rem'
-    }}>
-      <h1 style={h1Style}>Challenge Project</h1>
+    <header>
+      <h1>Challenge Project</h1>
       </header>
   )
 }
